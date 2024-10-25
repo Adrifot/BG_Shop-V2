61450bd0e182a68f7980cf10362a763e88c51ae8
Adrian Fota <adrian.mihael.fota@gmail.com>
2024-10-25 23:25:01 +0300

    Migrated to PostgreSQL.

--- models/boardgame.js
+++ models/boardgame.js
@@ -1,97 +1,120 @@
-const mongoose = require("mongoose");
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
 
-const BoardgameSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "Producte name required."]
+
+const Boardgame = sequelize.define(
+    "boardgame",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+
+        price: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+            validate: {
+                min: 0.01,
+                isFloat: {
+                    msg: "Price must be a valid number."
+                }
+            }
+        },
+
+        category: {
+            type: DataTypes.ENUM("Strategy", "Party", "Card Game", "Classic", "RPG", "Family", "Uncategorized"),
+            allowNull: false
+        },
+
+        tags: {
+            type: DataTypes.ARRAY(DataTypes.STRING),
+            allowNull: true
+        },
+
+        imagesrc: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        stock: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 0
+            }
+        },
+
+        rating: {
+            type: DataTypes.FLOAT,
+            defaultValue: 0,
+            validate: {
+                min: 0,
+                max: 5
+            }
+        },
+
+        minplayers: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 1
+            }
+        },
+
+        maxplayers: {
+            type: DataTypes.INTEGER,
+            validate: {
+                min: 1
+            }
+        },
+
+        playtime: {
+            type: DataTypes.INTEGER
+        },
+
+        age: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+
+        designer: {
+            type: DataTypes.STRING
+        },
+
+        publisher: {
+            type: DataTypes.STRING
+        },
+
+        releaseyear: {
+            type: DataTypes.INTEGER
+        },
+
+        isavailable: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true,
+            allowNull: false
+        },
+
+        isexpansion: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+            allowNull: false
+        }
     },
+    {
+        timestamps: true
+    }
+);
 
-    description: {
-        type: String,
-        required: [true, "Product description required."]
-    },
-
-    price: {
-        type: Number,
-        required: [true, "Product price required."],
-        min: [0.1, "Price must be positive"]
-    },
-
-    category: {
-        type: String,
-        enum: ["Strategy", "Party", "Card Game", "Classic", "RPG", "Family", "Uncategorized"],
-        required: [true, "Boardgame category required."]
-    },
-
-    tags: {
-        type: [String]
-    },
-
-    imageUrl: {
-        type: String,
-        required: [true, "Image URL required."]
-    },
-
-    /*stock: {
-        type: Number,
-        //required: true [WILL ADD THIS LATER]
-        min: [0, "Stock quantity can't be negative."]
-    },*/
-
-    /*rating: {
-        type: Number,
-        default: 0,
-        min: 1,
-        max: 5
-    },*/
-
-    minPlayers: {
-        type: Number,
-        //required: true,
-        min: 1
-    },
-
-    maxPlayers: {
-        type: Number,
-        //required: true,
-        min: 1
-    },
-
-    playtime: {
-        type: String
-    },
-
-    age: {
-        type: String,
-        //required: true
-    },
-
-    designer: {
-        type: String
-    },
-
-    publisher: {
-        type: String
-    },
-
-    releaseYear: {
-        type: Number
-    },
-
-    isAvailable: {
-        type: Boolean,
-        default: true
-    },
-
-    isExpansion: {
-        type: Boolean,
-        default: false
-    },
-
-    /*reviews: {
-        // User schema reference, will add later
-    },*/
-    
-}, {timestamps: true});
-
-module.exports = mongoose.model("Boardgame", BoardgameSchema);
+module.exports = Boardgame;
--- seeds/dbSeed.js
+++ seeds/dbSeed.js
@@ -1,39 +1,42 @@
-const mongoose = require("mongoose");
+const sequelize = require("../config/database");
 const Boardgame = require("../models/boardgame");
 const {gameName1, gameName2, categories, tags} = require("./gameseeds");
-
-mongoose.connect("mongodb://localhost:27017/bgshop");
-
-const db = mongoose.connection;
-
-db.on("error", console.error.bind(console, "Database connection error: "));
-db.once("open", () => {
-    console.log("MongoDB connected.");
-});
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
-    await Boardgame.deleteMany({});
-    for(let i=0; i<25; i++) {
-        let minPl = Math.floor(Math.random() * (5-1) + 1);
-        let maxPl = Math.floor(Math.random() * (7-minPl) + minPl);
-        const newBoardgame = new Boardgame({
-            name: `${sample(gameName1)} ${sample(gameName2)}`,
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem atque earum nesciunt molestiae. Blanditiis impedit perferendis quidem reiciendis vel enim, cupiditate nesciunt unde reprehenderit mollitia, at neque placeat sit quasi \
-                            Ab deserunt modi exercitationem mollitia a, rerum, facere laborum ipsum sed asperiores nobis unde quasi magni veniam, minus molestiae cumque recusandae! Veniam ipsum odio officia? Voluptas corrupti fugiat labore? Quasi.",
-            price: Math.floor(Math.random() * (300 - 10) + 10),
-            category: `${sample(categories)}`,
-            tags: `${sample(tags)}`,
-            imageUrl: `https://picsum.photos/300?random=${Math.random()}`,
-            minPlayers: minPl,
-            maxPlayers: maxPl,
-            releaseYear: Math.floor(Math.random() * (2024 - 1985) + 1985)
-        });
-        await newBoardgame.save();
+    try {
+        await sequelize.authenticate();
+        console.log("Sequelize connected.");
+        await Boardgame.destroy({where: {}, truncate: true});
+        for(let i=0; i<25; i++) {
+            let minPl = Math.floor(Math.random() * (5-1) + 1);
+            let maxPl = Math.floor(Math.random() * (7-minPl) + minPl);
+            const newBoardgame = {
+                name: `${sample(gameName1)} ${sample(gameName2)}`, //'Bandit Adventure',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit...',
+                price: Math.floor(Math.random() * (300 - 10) + 10),//84,
+                category: `${sample(categories)}`, //'Uncategorized',
+                stock: 1,
+                age: 12,
+                imagesrc: `https://picsum.photos/300?random=${Math.random()}`, //'https://picsum.photos/300?random=0.16288263637136824',
+                minplayers: minPl,
+                maxplayers: maxPl,
+                releaseyear:Math.floor(Math.random() * (2024 - 1985) + 1985), // 1996,
+                rating: 0, 
+                isavailable: true,
+                isexpansion: false,
+            };
+            await Boardgame.create(newBoardgame);
+        }
+    } catch(err) {
+        console.error("Error seeding database: ", err);
+    } finally {
+        await sequelize.close();
+        console.log("DB connection closed.");
     }
 }
 
-seedDB().then(() => {
-    mongoose.connection.close();
-});
+console.log("Seeding process initialized...");
+seedDB();
+
