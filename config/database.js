4eff77a39e8e7c45373f9dfe989fcd55676fd328
Adrian Fota <161955777+Adrifot@users.noreply.github.com>
2024-10-26 19:44:01 +0300

    Delete config directory

--- config/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize("bgshopv2", "bgshop_admin", "pZeb#rul24l", {
-    host: "localhost",
-    dialect: "postgres",
-});
-
-module.exports = sequelize;
