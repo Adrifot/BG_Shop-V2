774c7a4a8a76c9c252d34f12b0ee9984737f29cc
Adrian Fota <adrian.mihael.fota@gmail.com>
2025-06-07 22:23:39 +0300

    Fixed boardgame id not being found in review route

--- routers/reviewRouter.js
+++ routers/reviewRouter.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router({mergeParams: true});
 
 const asyncHandler = require("../utils/asyncHandler");
 const ExpressError = require("../utils/expressError");
@@ -8,8 +8,9 @@ const {Boardgame, Review} = require("../models");
 
 router.post("/", asyncHandler(async (req, res, next) => {
     if (!req.body.review) throw new ExpressError("Invalid data", 400);
+    const game = await Boardgame.findByPk(req.params.id);
+    if (!game) throw new ExpressError("Boardgame not found", 404);
     try {
-        const game = await Boardgame.findByPk(req.params.id);
         await Review.create({...req.body.review, boardgameId: game.id});
         res.redirect(`/boardgames/${game.id}`);
     } catch(err) {
