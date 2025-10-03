1f53763606a5cdf210e3dc0d1f8f6ee48052b724
Adrian Fota <adrian.mihael.fota@gmail.com>
2025-10-03 20:11:53 +0300

    Added basic register logic

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
+const User = require("../models/user");
+const asyncHandler = require("../utils/asyncHandler");
 
 router.get("/login", (req, res) => {
     res.render("pages/login");
+});
+
+router.get("/register", (req, res) => {
+    res.render("pages/register");
 });
 
 router.post('/login', 
@@ -12,5 +18,21 @@ router.post('/login',
         res.redirect('/');
     }
 );
+
+router.post("/register", asyncHandler(async (req, res, next) => {
+    const newUser = req.body.newuser;
+    newUser.pswdhash = newUser.password; // temporary, used for testing only
+
+    const existingUser = await User.findOne({ where: { username: newUser.username } });
+    if (existingUser) {
+        throw new ExpressError("Username already taken", 400);
+    }
+
+    const user = await User.create(newUser);
+    req.login(user, (err) => {
+        if (err) return next(err);
+        res.redirect("/");
+    });
+}));
 
 module.exports = router;
