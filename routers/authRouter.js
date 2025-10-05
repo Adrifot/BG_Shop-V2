8eab136dfd58d920e1329c785f3d2e0cef8c8720
Adrian Fota <adrian.mihael.fota@gmail.com>
2025-10-05 23:30:38 +0300

    Added logout route

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -4,21 +4,31 @@ const passport = require("passport");
 const User = require("../models/user");
 const asyncHandler = require("../middleware/asyncHandler");
 
-router.get("/login", (req, res) => {
-    res.render("pages/users/login");
-});
-
 router.get("/register", (req, res) => {
     res.render("pages/users/register");
-});
-
-router.post('/login', passport.authenticate('local', { failureRedirect: '/login' }), (req, res) => {
-    res.redirect('/');
 });
 
 router.post("/register", asyncHandler(async (req, res) => {
     await User.register(req.body.newuser);
     res.redirect("/login");
 }));
+
+router.get("/login", (req, res) => {
+    res.render("pages/users/login");
+});
+
+router.post("/login", 
+    passport.authenticate("local", { failureRedirect: "login" }), 
+        (req, res) => {
+            res.redirect("/");
+        }
+);
+
+router.get("/logout", (req, res, next) => {
+    req.logout((err) => {
+        if (err) { return next(err); }
+        res.redirect("/home");
+    });
+});
 
 module.exports = router;
